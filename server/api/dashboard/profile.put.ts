e8b15a07008bbc52ad9231871e4d5daeafdf8ecb
Boggy B <bbogdan14@gmail.com>
2026-03-18 15:55:11 +0000

    feat: add GET endpoints for profile and social links data

--- server/api/dashboard/profile.put.ts
+++ server/api/dashboard/profile.put.ts
@@ -1,4 +1,4 @@
-import { defineEventHandler, readBody } from 'h3'
+import { defineEventHandler, readBody, createError } from 'h3'
 import { z } from 'zod'
 import { requireUserSession } from '../../utils/auth'
 
